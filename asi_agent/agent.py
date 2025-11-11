72f46fcfefad7b4d7f00e59e99ca574683aedf22
Ishita Agarwal <ishitagrawal0207@gmail.com>
2025-11-11 21:14:38 +0530

    Update agent.py

--- asi_agent/agent.py
+++ asi_agent/agent.py
@@ -9,7 +9,7 @@ agent = Agent(
     name="risk_score_agent",
     port=8001,
     seed="risk_agent_seed_phrase_123",
-    endpoint=["http://localhost:8001/submit"],
+    endpoint=["https://invoicesme.onrender.com/submit"],
     mailbox=True
 )
 
